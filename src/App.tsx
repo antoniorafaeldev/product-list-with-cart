7471d6a308477184b2b46c5723c33587cbcb13bb
Antônio Rafael <antonio.rafael0415@gmail.com>
2026-08-14 20:11:41 -0300

    feat: product component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+
+import { Product } from "./components/layout/Product";
 import "./style/theme.css";
 
 function App() {
-  return <h1>OI</h1>;
+  return (
+    <>
+      < Product image="" category="Waffle" price="5.67" />
+    </>);
 }
 
 export default App;
---
+++ src/components/layout/Product.tsx
@@ -0,0 +1,13 @@
+import type { Product } from "../../interfaces/product";
+
+export function Product({ image, category, title, price }: Product) {
+  return (
+    <div>
+      <img src={image} alt={`Imagem de ${image}`} />
+      <button>Add to Cart</button>
+      <span>{category}</span>
+      <h2>{title}</h2>
+      <span>${price}</span>
+    </div>
+  );
+}
