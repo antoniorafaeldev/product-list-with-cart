70126476d12b02bc48996e4135b6077614066d19
Antônio Rafael <antonio.rafael0415@gmail.com>
2026-08-15 14:22:06 -0300

    feat: button component

---
+++ src/components/shared/Button.tsx
@@ -0,0 +1,8 @@
+export function Button(){
+    return (
+        <button>
+            <img src="./assets/images/icon-add-to-cart.svg" alt="" />
+            <span>Add to Cart</span>
+        </button>
+    )
+}
