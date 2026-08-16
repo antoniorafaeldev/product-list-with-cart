b0af8e1a09bb4b246203261ae6e13f8e512356eb
Antônio Rafael <antonio.rafael0415@gmail.com>
2026-08-16 11:22:55 -0300

    style: desktop design

--- src/components/layout/Cart.tsx
+++ src/components/layout/Cart.tsx
@@ -1,6 +1,6 @@
 export function Cart(){
     return (
-        <section className="bg-rose-50 p-4 rounded-lg">
+        <section className="bg-rose-50 p-4 rounded-lg lg:h-fit">
             <h2 className="text-red-500 text-2xl font-bold mb-8">Your Cart (0)</h2>
             <img className="m-auto" src="./assets/images/illustration-empty-cart.svg" alt="Image of a sliced cake, signaling your cart is emptty" />
             <p className="text-center text-rose-500 font-medium my-4">Your added items will appear here </p>
--- src/components/layout/Product.tsx
+++ src/components/layout/Product.tsx
@@ -5,7 +5,7 @@ export function Product({ image, category, name, price }: ProductProps) {
   return (
     <div className="py-2">
       <img
-        className="rounded-lg lg:w-2xs"
+        className="rounded-lg lg:w-xs xl:w-2xs"
         src={image.mobile}
         alt={`Imagem of ${name}`}
       />
