b05063d4fdaec85bd94a420fd4efa9b5d4cf611c
Antônio Rafael <antonio.rafael0415@gmail.com>
2026-08-19 18:15:30 -0300

    style: hover effects for buttons

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -1,5 +1,5 @@
 const buttonClasses: string =
-  "absolute flex left-1/2 gap-1 -translate-x-1/2 -translate-y-6 bg-rose-50 px-4 py-2 rounded-xl text-center top-0 m-auto border-1 border-rose-500";
+  "absolute flex left-1/2 gap-1 -translate-x-1/2 -translate-y-6 bg-rose-50 px-4 py-2 rounded-xl text-center top-0 m-auto border-1 border-rose-500 cursor-pointer transition-all duration-300 hover:text-red-500 hover:border-red-500 hover:scale-105 active:scale-95";
 
 export function Button() {
   return (
